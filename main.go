4092f93ee89014ce9825103d6d41f4b40bd75465
NishanthSpShetty <nishanthspshetty@gmail.com>
2021-03-25 21:42:05 +0530

    setup main with the router

--- main.go
+++ main.go
@@ -1,5 +1,65 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	h "github.com/hex_url_shortner/api"
+	mr "github.com/hex_url_shortner/reposiory/mongodb"
+	rr "github.com/hex_url_shortner/reposiory/redis"
+	"github.com/hex_url_shortner/shortner"
+)
+
+func main() {
+	repo := chooseRepo()
+	service := shortner.NewRedirectService(repo)
+	handler := h.NewHandler(service)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{code}", handler.Get)
+	r.Post("/", handler.Post)
+}
+
+func chooseRepo() shortner.RedirectRepository {
+	switch os.Getenv("URL_DB") {
+	case "redis":
+		redisUrl := os.Getenv("REDIS_URL")
+		repo, err := rr.NewRedisRepository(redisUrl)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+
+	case "mongo":
+		mongoURL := os.Getenv("MONGO_URL")
+		mongoDB := os.Getenv("MONGO_DB")
+		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
+
+		repo, err := mr.NewMongoRepository(mongoURL, mongoDB, mongoTimeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return repo
+	}
+	return nil
+}
+
+func httpPort() string {
+	port := "8080"
+
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = envPort
+	}
+
+	return fmt.Sprintf(":%s", port)
 }
